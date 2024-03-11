3cffad23cab8c139c19e59f1afc88812cd306b36
asbin12 <140784814+asbin12@users.noreply.github.com>
2024-03-11 19:53:03 +0545

    Todo List

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,9 @@
-// import Resturants from './components/Basics/Resturants'
-// import Counter from './components/counter/Counter'
-// import UseEffectExample from './components/counter/UseEffectExample'
-// import UseReducerExam from './components/counter/UseReducerExam'
-
-// import { useState } from "react";
-// import Practice from "./components/Practice/Practice"
-// import Weather from "./components/weather/Weather"
-// import FooterCart from './components/weather/FooterCart'
-// import Weather from "./components/weather/Weather";
 import TodoList from "./components/Todo/TodoList";
-import "./components/weather/navbar.css";
-
-// import TodoList from "./components/Todo/TodoList"
 
 function App() {
   return (
     <>
-      {/* <Counter/> */}
-   
-      {/* <Resturants/> */}
-      <TodoList/>
-      {/* <Weather /> */}
-      {/* <FooterCart/> */}
-      {/* <Practice/> */}
+      <TodoList />
     </>
   );
 }
