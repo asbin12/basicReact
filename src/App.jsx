08f7f6fe474c2bd764209f6ae6f7210c6b45a421
asbin12 <140784814+asbin12@users.noreply.github.com>
2024-03-11 19:44:30 +0545

    todo counter resturants

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,30 @@
-
-
 // import Resturants from './components/Basics/Resturants'
 // import Counter from './components/counter/Counter'
 // import UseEffectExample from './components/counter/UseEffectExample'
 // import UseReducerExam from './components/counter/UseReducerExam'
 
 // import { useState } from "react";
-import Weather from "./components/weather/Weather"
+// import Practice from "./components/Practice/Practice"
+// import Weather from "./components/weather/Weather"
 // import FooterCart from './components/weather/FooterCart'
-import './components/weather/navbar.css'
+// import Weather from "./components/weather/Weather";
+import TodoList from "./components/Todo/TodoList";
+import "./components/weather/navbar.css";
 
 // import TodoList from "./components/Todo/TodoList"
-
 
 function App() {
   return (
     <>
       {/* <Counter/> */}
-      {/* <UseReducerExam /> */}
-      {/* <UseEffectExample /> */}
-      {/* <Resturants/> */}
-      {/* <TodoList/> */}
-      <Weather />
-      {/* <FooterCart/> */}
    
+      {/* <Resturants/> */}
+      <TodoList/>
+      {/* <Weather /> */}
+      {/* <FooterCart/> */}
+      {/* <Practice/> */}
     </>
-  )
+  );
 }
 
-
-export default App
+export default App;
